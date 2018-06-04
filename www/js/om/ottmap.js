0ad9acaeccc0e37d41048763f3b5cfaf812adda5
Albert Figueras <afigueras21@me.com>
2018-06-04 11:56:17 +0200

    afegit comentaris a ott

--- www/js/om/ottmap.js
+++ www/js/om/ottmap.js
@@ -26,16 +26,6 @@ function setValorInProgress(){
 function afegeixPoble(comarca, pobles, entry){
 	
 	var p = new Poble(entry['gsx$entitat'].$t);
-	/*p.descripcio.push(new Detall("Entitat", entry['gsx$entitat'].$t));
-	p.descripcio.push(new Detall("Tipus entitat", entry['gsx$tipusentitat'].$t));
-	p.descripcio.push(new Detall("Nom comercial", entry['gsx$nomcomercial'].$t));
-	p.descripcio.push(new Detall("URL", entry['gsx$url'].$t));
-	p.descripcio.push(new Detall("Programació", entry['gsx$programació'].$t));
-	p.descripcio.push(new Detall("Canal TDT", entry['gsx$canaltdt'].$t));
-	p.descripcio.push(new Detall("Xarxa", entry['gsx$xarxa'].$t));
-	p.descripcio.push(new Detall("Youtube", entry['gsx$youtube'].$t));
-	p.descripcio.push(new Detall("Població", entry['gsx$població'].$t));
-	p.descripcio.push(new Detall("Observacions", entry['gsx$observacions'].$t));*/
 	p.descripcio.push(new Detall("Entitat", entry['gsx$entitat'].$t));
 	p.descripcio.push(new Detall("Tipus", entry['gsx$tipus'].$t));
 	p.descripcio.push(new Detall("Directe URL", entry['gsx$directeurl'].$t));
@@ -183,6 +173,9 @@ function togglemapform(){
 function load(){
 	$("#pinmap").click(function(){
 		togglemapform();
+	});
+	$("#comico").click(function(){
+		$("#gform").toggle("slow");
 	});
 	console.log("carregat");
 }
